2827620b878f8c97b437a8fc19f3688452f2bf47
YoussefLMT <mcyoussef324@gmail.com>
2022-08-25 20:20:09 +0100

    add Employee model and relationship

---
+++ app/Models/Employee.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+       'first_name',
+       'last_name',
+       'email',
+       'phone',
+       'job',
+       'salary',
+       'department_id'
+    ];
+
+
+    public function department()
+    {
+        return $this->hasOne(Department::class);
+    }
+}
