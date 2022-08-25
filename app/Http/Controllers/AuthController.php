7ab4bf732b629142274963d8429e2e0c831ba71b
YoussefLMT <mcyoussef324@gmail.com>
2022-08-25 21:19:42 +0100

    add login function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,13 +10,47 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function register(Request $req){
-        
-        $validator = Validator::make($req->all(), [
-            'name' => 'required',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required',
 
+    // public function register(Request $req){
+        
+    //     $validator = Validator::make($req->all(), [
+    //         'name' => 'required',
+    //         'email' => 'required|email|unique:users,email',
+    //         'password' => 'required',
+
+    //     ]);
+
+    //     if($validator->fails()){
+
+    //         return response()->json([
+    //             'validation_err' => $validator->messages(),
+    //         ]);
+
+    //     }else{
+
+    //         $user = User::create([
+    //             'name' => $req->name,
+    //             'email' => $req->email,
+    //             'password' => Hash::make($req->password),
+    //         ]);
+
+
+    //         return response()->json([
+    //             'status' => 200,
+    //             'message' => 'User created successfully',
+    //         ]);
+            
+    //     }
+
+    // }
+
+
+
+    public function login(Request $req){
+
+        $validator = Validator::make($req->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
         ]);
 
         if($validator->fails()){
@@ -27,20 +61,31 @@ class AuthController extends Controller
 
         }else{
 
-            $user = User::create([
-                'name' => $req->name,
-                'email' => $req->email,
-                'password' => Hash::make($req->password),
-            ]);
+            $user = User::where('email', $req->email)->first();
+ 
+            if(!$user || !Hash::check($req->password, $user->password)) {
 
+                return response()->json([
+                    'status' => 401,
+                    'message' => 'Invalid Login!, Please Try Again',
+                ]);
 
-            return response()->json([
-                'status' => 200,
-                'message' => 'User created successfully',
-            ]);
-            
+            }else{
+
+                $token = $user->createToken($user->email.'_token', ['server:admin'])->plainTextToken;
+
+                return response()->json([
+                    'status' => 200,
+                    'token' => $token,
+                    'message' => 'You are logged in successfully',
+                ]);
+
+         
+            }
+         
         }
 
     }
+
 
 }
