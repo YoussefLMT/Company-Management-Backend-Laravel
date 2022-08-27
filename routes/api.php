a962287a55860122fdeeaf281a42d2fdc2f54d43
YoussefLMT <mcyoussef324@gmail.com>
2022-08-27 20:02:53 +0100

    add router for departments CRUD

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DepartmentsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,13 @@ use App\Http\Controllers\AuthController;
 // Route::post('register', [AuthController::class, 'register']);
 
 Route::post('login', [AuthController::class, 'login']);
+
+
+Route::get('departments', [DepartmentsController::class, 'getDepartments']);
+Route::post('add-department', [DepartmentsController::class, 'addDepartments']);
+Route::get('get-department/{id}', [DepartmentsController::class, 'getDepartment']);
+Route::put('update-department/{id}', [DepartmentsController::class, 'updateDepartments']);
+Route::delete('delete-department/{id}', [DepartmentsController::class, 'deleteDepartments']);
 
 
 
