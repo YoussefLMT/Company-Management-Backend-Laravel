5c090887bdeadd1028fcb71c33011a12fe84b6af
YoussefLMT <mcyoussef324@gmail.com>
2022-08-27 20:09:05 +0100

    some modifications

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\DepartmentsController;
+use App\Http\Controllers\DepartmentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,11 +22,11 @@ use App\Http\Controllers\DepartmentsController;
 Route::post('login', [AuthController::class, 'login']);
 
 
-Route::get('departments', [DepartmentsController::class, 'getDepartments']);
-Route::post('add-department', [DepartmentsController::class, 'addDepartments']);
-Route::get('get-department/{id}', [DepartmentsController::class, 'getDepartment']);
-Route::put('update-department/{id}', [DepartmentsController::class, 'updateDepartments']);
-Route::delete('delete-department/{id}', [DepartmentsController::class, 'deleteDepartments']);
+Route::get('departments', [DepartmentController::class, 'getDepartments']);
+Route::post('add-department', [DepartmentController::class, 'addDepartment']);
+Route::get('get-department/{id}', [DepartmentController::class, 'getDepartment']);
+Route::put('update-department/{id}', [DepartmentController::class, 'updateDepartment']);
+Route::delete('delete-department/{id}', [DepartmentController::class, 'deleteDepartment']);
 
 
 
