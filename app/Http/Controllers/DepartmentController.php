46f0e7d336d0ca452d0ba147e589dc560d65ebd8
YoussefLMT <mcyoussef324@gmail.com>
2022-08-27 19:51:05 +0100

    add getDepartments function

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -1,10 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Department;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+
 
 class DepartmentController extends Controller
 {
-    //
+    
+    public function getDepartments()
+    {
+
+        $departments = Department::all();
+
+        return response()->json([
+            'status' => 200,
+            'departments' => $departments,
+        ]);
+    }
+
+    
 }
