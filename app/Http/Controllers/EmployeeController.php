a16a2a7f1da4224101f2ee9b48ed4df576e598ed
YoussefLMT <mcyoussef324@gmail.com>
2022-08-29 18:08:15 +0100

    add addEmployee function

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,8 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Employee;
+use Illuminate\Support\Facades\Validator;
+
 
 class EmployeeController extends Controller
 {
-    //
+    
+    public function addEmployee(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name'=> 'required',
+            'last_name'=> 'required',
+            'email' => 'required|email|unique:employees,email',
+            'phone' => 'required',
+            'job' => 'required',
+            'salary' => 'required',
+            'department_id' => 'required',
+        ]);
+
+        if($validator->fails()){
+
+            return response()->json([
+                'validation_err' => $validator->messages(),
+            ]);
+
+        }else{
+
+            Employee::create([
+                'first_name'=> $request->first_name,
+                'last_name' => $request->last_name,
+                'email'=> $request->email,
+                'phone' => $request->phone,
+                'job' => $request->job,
+                'salary' => $request->salary,
+                'department_id' => $request->department_id
+            ]);
+    
+            return response()->json([
+                'status' => 200,
+                'message' => "Employee added successfully",
+            ]);
+        }
+    }
 }
