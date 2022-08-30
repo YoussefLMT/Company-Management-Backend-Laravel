e9b9f4f17d31f50ce2ad249d6b7bc0467c208966
YoussefLMT <mcyoussef324@gmail.com>
2022-08-30 19:51:57 +0100

    add route for get profile

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::post('logout', [AuthController::class, 'logOut']);
 
+    Route::get('get-profile', [UserController::class, 'getProfile']);
 
 });
 
