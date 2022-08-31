50395c4b03a05ce3e8076fae1017b44cf3339be9
YoussefLMT <mcyoussef324@gmail.com>
2022-08-31 18:23:36 +0100

    add getTotalCount function

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Employee;
+use App\Models\Customer;
+use App\Models\Department;
 
 class StatisticsController extends Controller
 {
-    //
+    
+    public function getTotalVisitsCount(){
+
+        $employeesCount = Employee::all()->count();
+        $customersCount = Customer::all()->count();
+        $departmentsCount = Department::all()->count();
+
+        return response()->json([
+            'status' => 200,
+            'employeesCount' => $employeesCount,
+            'customersCount' => $customersCount,
+            'departmentsCount' => $departmentsCount,
+        ]);
+    }
 }
